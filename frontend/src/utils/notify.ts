50b034b4c507186e948e60c3c0d4f7c1f23b6f68
ritwik-69 <ritwikpatnaik@gmail.com>
2024-10-07 15:02:35 +0530

    Added notify option

--- frontend/src/utils/notify.ts
+++ frontend/src/utils/notify.ts
@@ -3,6 +3,6 @@ import toast from "react-hot-toast";
 export function notify(message: string) {
   toast(message, {
     duration: 5000,
-    position: "top-right",
+    position: "top-center",
   });
 }
